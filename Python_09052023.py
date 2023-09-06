93f8b031a557c49c466769689c2f513129eb89c5
School Software <schoolsoftware@macbook-air.american.edu>
2023-09-06 16:43:06 -0400

    python practice

---
+++ Python_09052023.py
@@ -0,0 +1,14 @@
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#import seaborn as sns
+#import statsmodels as sm
+
+
+print("Hello World")
+
+tup = 4,5,6
+tup
+
+nested_tup = (4,5,6),(7,8)
+nested_tup
